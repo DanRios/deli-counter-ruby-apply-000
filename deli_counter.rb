1d31990a23a37b6045281cbb6bb1e7a5944f8915
Daniel Rios <danielrafaelrios@gmail.com>
2017-10-19 23:23:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,30 +1,22 @@
-# Write your code here
 katz_deli = []
 
-def line(katz_deli)
-line = ""
-count = 1
-katz_deli.each do |name|
-  line += " #{count}. #{name}"
-  count += 1
-end
-if katz_deli.empty?
-  puts "The line is currently empty."
-else
-  puts "The line is currently:#{line}"
-end
+def take_a_number(katz_deli)
+if katz_deli.length == 0
+  number = rand(0..100)
+  katz_deli << number
+  else
+    add = katz_deli.last + 1
+    katz_deli << add
+    end
+  puts "You are currently number #{katz_deli.last} in line."
 end
 
-def take_a_number(katz_deli, name)
-  katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-end
 
 def now_serving(katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli.first}."
-    katz_deli.shift
+    puts "Currently serving #{katz_deli.shift}."
   end
+  katz_deli
 end
